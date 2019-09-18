bf6a9d6a725a6738f65720b9a47a62e23041acfe
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-18 17:36:14 -0300

    Initial commit for notes app. Created some commands with Yargs.

---
+++ app.js
@@ -0,0 +1,46 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
