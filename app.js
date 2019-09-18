11ac6a79f75963a80cb609ffb62c7034b35ba06c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-18 19:33:27 -0300

    Created options for add command

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('title: ' + argv.title)
+        console.log('body: ' + argv.body)
     }
 })
 
@@ -41,6 +54,4 @@ yargs.command({
     }
 })
 
-// add, remove, read, list
-
-console.log(yargs.argv)
+yargs.parse()
